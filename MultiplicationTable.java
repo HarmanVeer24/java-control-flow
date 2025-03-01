2d8a9ccdfb4fc10cb661a1ab4782f6da97e9c53e
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-01 14:32:59 +0530

    [Harman] ADD:Program to print mutiple of given input from 6 to 9

---
+++ MultiplicationTable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt user for input
+        int number = scanner.nextInt();
+        
+        // Generate multiplication table from 6 to 9
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }       
+    }
+}
