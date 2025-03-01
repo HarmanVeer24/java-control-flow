01953ef1d631aa57d83282ce73dcaaba3e6abb62
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-01 14:40:23 +0530

    [Harman] ADD:program to print countdown for rocket launch using for loop

---
+++ RocketLaunchCountdownForLoop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RocketLaunchCountdownForLoop {
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt the user to enter a countdown start value
+        int counter = scanner.nextInt();
+        
+        // Countdown using a while loop
+        for(int i=counter;i>=1;i--{
+            System.out.println(counter);
+        }
+    }
+}
