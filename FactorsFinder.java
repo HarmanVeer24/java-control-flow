8d05f46280b271d138cc2d32e27ead8d5800cedf
HarmanVeer24 <harmanveer0024@gmail.com>
2025-03-02 19:16:50 +0530

    [Harman] ADD:program to print factors of given number

---
+++ FactorsFinder.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FactorsFinder {
+    public static void main(String[] args) {
+        // Create a Scanner object for user input
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt user for input
+        int number = scanner.nextInt();
+        
+        // Check if the input is a positive integer
+        if (number > 0) {
+            System.out.println("Factors of " + number + " are:");
+            // Find and print factors
+            for (int i = 1; i <= number; i++) {
+                if (number % i == 0) {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("Please enter a positive integer.");
+        }
+        
+    }
+}
